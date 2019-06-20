1f27420dc32ae6e2f211e7fc90089f8f743477fc
Diogo <diogo-ramalho92@hotmail.com>
2019-06-20 16:55:26 +0100

    solo paages

---
+++ Movie.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>Watch /Fill Blank/</title>
+        <link href="css/css-projeto.css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+        <link href="css/bootstrap.css" rel="stylesheet">
+    </head>
+  
+    <body style="margin-top: 0;">
+  
+      <section class="section-barra">
+        <nav class="barra">
+          <a class="brand-barra" href="index.php"><i class="fas fa-bolt" style="font-size: 20px; color: rgb(250, 188, 96)"></i> ThunderStream</a>
+          <a class="link-barra" href="Login.php">Log In</a>
+          <a class="link-barra" href="SignUp.php">Sign Up</a>
+        </nav>
+      </section>
+
+      <section class="page-container">
+        <section class="content-wrap">
+            <section class="container">
+                <section class="movie_individual">
+                    <section style="padding: 15px 0; background-color: rgba(0, 0, 0, 0.75);">
+                        <p class="movie_title">Movie Title</p>
+                    </section>
+                      <section class="row">
+                        <section class="col-lg-4 col-md-6 mb-4" style="padding: 0; margin-left: 2rem; margin-top: 1rem;">
+                            <img class="movie_image" src="https://iphonewalls.net/wp-content/uploads/2013/05/Fight%20Club%20iPhone%20Wallpaper.jpg">
+                            <button type="button" class="movie_trailer">Trailer</button>
+                            <button type="button" class="movie_fav">Favorite</button>
+                            <button type="button" class="movie_play">Play</button>
+                        </section>
+                        <section class="col-lg-7 col-md-9 mb-4" style="padding: 0; margin-left: 15px; margin-top: .75rem">
+                            <p class="movie_text"><b>Descrição:</b> Descrição</p>
+                            <br>
+                            <p class="movie_text"><b>Produtor:</b> Produtor</p>
+                            <p class="movie_text"><b>Ano de lançamento:</b> Ano</p>
+                            <!--<p class="movie_text"><b>Texto:</b> Texto</p>--> 
+                        </section>
+                      </section>
+                </section>
+            </section>
+        </section>
+      </section>
+
+    </body>
+
+</html>
